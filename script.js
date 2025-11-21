df6cb26e2839402f9ae5ea8b405a1948127651f6
Your Alex <alexschoenthaler@gmail.com>
2025-11-21 08:42:41 +0100

    style chanched, load more Pokemons added

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const BASE_URL = "https://pokeapi.co/api/v2/";
+
 let content = document.getElementById('content');
 let pokemonsDetail = [];
+let pokemonIndex = 30;
+let addedNewPokemons = 31;
+
 
 async function init() {
-    await renderPokemons();
+    await StartPokemons();
     // console.log(pokemonsDetail);
 }
 
 async function GetPokemons(path) {
-
    try {
     let response = await fetch(path);
     let responseAsJson = await response.json();
@@ -20,26 +22,51 @@ async function GetPokemons(path) {
     
 }
 
-async function renderPokemons() {
- let responseAsJson  = await GetPokemons("https://pokeapi.co/api/v2/pokemon?limit=31&offset=0");
- let pokemons = responseAsJson.results;
- 
-    for (let pokemonIndex = 0; pokemonIndex < pokemons.length; pokemonIndex++) {    
-       
-        pokemonsDetail.push(await GetPokemons(pokemons[pokemonIndex].url));
-        let pokemonType = GetPokemonTypes(pokemonIndex);
-        content.innerHTML += pokemonCard(pokemons[pokemonIndex].name, pokemonIndex, pokemonType[0].firstType, pokemonType[0].secondType);
+async function StartPokemons(){
+    let responseAsJson  = await GetPokemons("https://pokeapi.co/api/v2/pokemon?limit=31&offset=0");
+    let pokemons = responseAsJson.results;
+    let pokemonindex = -1;
+    renderPokemons(pokemons, pokemonindex);
+}
+
+async function renderPokemons(pokemons, pokemonindex) {
+    for (let index = 0; index < pokemons.length; index++) {    
+        pokemonindex++;
+        pokemonsDetail.push(await GetPokemons(pokemons[index].url));
+        let pokemonType = GetPokemonTypes(pokemonindex);
+        content.innerHTML += pokemonCard(pokemons[index].name, pokemonindex, pokemonType[0].firstType, pokemonType[0].secondType);
     }
     
 }
 
-function GetPokemonTypes(pokemonIndex) {
+function GetPokemonTypes(index) {
     let pokemonTypes = []
-    let pokemonFirstType = pokemonsDetail[pokemonIndex].types[0].type.name;
-    let pokemonSecondType = pokemonsDetail[pokemonIndex].types[1] === undefined ? "" : pokemonsDetail[pokemonIndex].types[1].type.name;
+    let pokemonFirstType = pokemonsDetail[index].types[0].type.name;
+    let pokemonSecondType = ``;
+    switch (pokemonsDetail[index].types[1]) {
+        case undefined:      
+            break;
+        default:
+            pokemonSecondType = `../assets/img/PokemonTypes/${pokemonsDetail[index].types[1].type.name}.png`;
+            break;
+    }
     pokemonTypes.push({
         firstType:pokemonFirstType,
         secondType:pokemonSecondType
     })
     return pokemonTypes;
+}
+
+async function LoadMorePokemons() {
+    let responseAsJson  = await GetPokemons(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${addedNewPokemons}`);
+    let pokemons = responseAsJson.results;
+    console.log(pokemons);
+    renderPokemons(pokemons, pokemonIndex);  
+    addedNewPokemons+=20;
+    pokemonIndex +=20;
+}
+
+function SearchPokemons(){
+    
+    
 }
--- scripts/CardTamplate.js
+++ scripts/CardTamplate.js
@@ -2,8 +2,8 @@ function pokemonCard(pokemonNAME, pokemonNUMBER, pokemonFirstType, pokemonSecond
     return `<div class = "card type-${pokemonFirstType}">
             <img src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemonNUMBER +1}.svg" alt="Pokemon">
             <span>${pokemonNAME}</span>
-            <img src="./assets/img/PokemonTypes/${pokemonFirstType}.png" alt="${pokemonFirstType}">
-            <img src="./assets/img/PokemonTypes/${pokemonSecondType}.png" alt="${pokemonSecondType}">
+            <img src="../assets/img/PokemonTypes/${pokemonFirstType}.png" alt="${pokemonFirstType}">
+            <img src ="${pokemonSecondType}">
             </div>`
 
 }
